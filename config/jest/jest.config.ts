4683bf8c6446f8992d7d8add01a39f5723e1e8e4
Veronika Kuznetsova <djveronika@mail.ru>
2024-05-23 17:13:02 +0700

    add jest, test for ClassNames

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,22 @@
+export default {
+  clearMocks: true,
+  testEnvironment: 'jsdom',
+  coveragePathIgnorePatterns: [
+    '\\\\node_modules\\\\',
+  ],
+  moduleDirectories: [
+    "node_modules"
+  ],
+  moduleFileExtensions: [
+    "js",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node"
+  ],
+  testMatch: [
+    '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+  ],
+  rootDir: '../../',
+};
