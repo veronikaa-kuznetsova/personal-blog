1f7dfd303a4f30f2105c8a868a509d32c76ca902
Veronika Kuznetsova <djveronika@mail.ru>
2024-05-27 17:33:25 +0700

    add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,7 @@ module.exports = {
       'error',
       {
         markupOnly: true,
-        ignoreAttribute: ['data-testid'],
+        ignoreAttribute: ['data-testid', 'to'],
       },
     ],
     'max-len': ['error', { ignoreComments: true, code: 100 }],
