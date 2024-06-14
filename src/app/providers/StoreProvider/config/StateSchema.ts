8941465d276fb895d7678fba34f2c53840ba01a7
Veronika Kuznetsova <djveronika@mail.ru>
2024-06-14 14:14:24 +0700

    Instance API

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -2,9 +2,14 @@ import { CounterSchema } from 'entities/Counter';
 import { UserSchema } from 'entities/User';
 import { LoginSchema } from 'features/AuthByUsername';
 import {
-  AnyAction, CombinedState, EnhancedStore, Reducer, ReducersMapObject,
+  AnyAction, EnhancedStore, Reducer, ReducersMapObject,
 } from '@reduxjs/toolkit';
+import { CombinedState } from 'redux';
 import { ProfileSchema } from 'entities/Profile';
+import { AxiosInstance } from 'axios';
+// @ts-ignore
+import { To } from 'history';
+import { NavigateOptions } from 'react-router';
 
 export interface StateSchema {
  counter: CounterSchema;
@@ -26,4 +31,14 @@ export interface ReducerManager {
 
 export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
  reducerManager: ReducerManager;
+}
+
+export interface ThunkExtraArg {
+ api: AxiosInstance;
+ navigate: (to: To, options?: NavigateOptions) => void,
+}
+
+export interface ThunkConfig<T> {
+ rejectValue: T;
+ extra: ThunkExtraArg;
 }
