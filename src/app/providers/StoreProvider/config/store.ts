8078f48a788d691f7df172800d1229e57091de9e
Veronika Kuznetsova <djveronika@mail.ru>
2024-06-14 20:57:20 +0700

    add profile card, fix typing

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -2,10 +2,10 @@ import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { $api } from 'shared/api/api';
-import { NavigateOptions } from 'react-router';
-import { CombinedState, Reducer } from 'redux';
 // @ts-ignore
 import { To } from 'history';
+import { NavigateOptions } from 'react-router';
+import { CombinedState, Reducer } from 'redux';
 import { StateSchema, ThunkExtraArg } from './StateSchema';
 import { createReducerManager } from './reducerManager';
 
