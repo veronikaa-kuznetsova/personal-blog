82f96c64fea8012a91117429c6a542a80586a0cd
Veronika Kuznetsova <djveronika@mail.ru>
2024-06-25 17:49:56 +0700

    fix errors, add eslint plugin

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.lazy.ts
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.lazy.ts
@@ -1,5 +1,5 @@
 import { FC, lazy } from 'react';
-import { LoginFormProps } from 'features/AuthByUsername/ui/LoginForm/LoginForm';
+import { LoginFormProps } from '../LoginForm/LoginForm';
 
 // @ts-ignore
 export const LoginFormLazy = lazy<FC<LoginFormProps>>(() => import('./LoginForm'));
--- src/shared/ui/Select/Select.stories.tsx
+++ src/shared/ui/Select/Select.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Select } from 'shared/ui/Select/Select';
+import { Select } from './Select';
 
 export default {
   title: 'shared/Select',
