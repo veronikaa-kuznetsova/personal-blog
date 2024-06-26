59899798909e3d01a3d79840c361da2a4e3f3398
Veronika Kuznetsova <djveronika@mail.ru>
2024-06-26 16:09:24 +0700

    html reporter for unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -34,4 +34,13 @@ export default {
     '\\.s?css$': 'identity-obj-proxy',
     '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
   },
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      publicPath: '<rootDir>/reports/unit',
+      filename: 'report.html',
+      openReport: true,
+      inlineSource: true,
+    }],
+  ],
 };
