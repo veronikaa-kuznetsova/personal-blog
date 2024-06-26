6b700ac1bcb862c43df40bbaa5854204ef8a7409
Veronika Kuznetsova <djveronika@mail.ru>
2024-06-26 10:53:52 +0700

    remove purple theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -10,22 +10,10 @@ export function useTheme(): UseThemeResult {
   const { theme, setTheme } = useContext(ThemeContext);
 
   const toggleTheme = () => {
-    let newTheme: Theme;
-    switch (theme) {
-    case Theme.DARK:
-      newTheme = Theme.LIGHT;
-      break;
-    case Theme.LIGHT:
-      newTheme = Theme.PURPLE;
-      break;
-    case Theme.PURPLE:
-      newTheme = Theme.DARK;
-      break;
-    default:
-      newTheme = Theme.LIGHT;
-    }
+    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
     setTheme?.(newTheme);
     document.body.className = newTheme;
+
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
